e1b5941122dd200d9ae3fcaeabd3ea0035f9d970
Eliseu Dias <eliseudr@hotmail.com>
2020-08-09 22:24:09 -0300

    Criando layout para pagina HOME

--- src/Home.java
+++ src/Home.java
@@ -1,8 +1,14 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import sun.java2d.loops.DrawRect;
 
 /*
  * 
@@ -14,6 +20,36 @@ public class Home extends JFrame implements ActionListener {
 	
 	JFrame home = new JFrame();
 	
+	//Adicionando Layout
+	void HomeDesing() {
+		
+		JPanel visaoGeral = new JPanel();
+		JPanel transferencias = new JPanel();
+		JPanel historico = new JPanel();
+		JPanel fundo = new JPanel();
+		
+		visaoGeral.setBackground(Color.darkGray);
+		transferencias.setBackground(Color.darkGray);
+		historico.setBackground(Color.darkGray);
+		//Ultimo panel cobre o fundo todo
+		fundo.setBackground(Color.lightGray);
+		
+		//setSize controla a largura e altura do painel
+		visaoGeral.setBounds(20, 50, 0, 0);;
+		visaoGeral.setSize(new Dimension(970, 400));
+		
+		transferencias.setBounds(20, 470, 0, 0);
+		transferencias.setSize(new Dimension(475, 240));
+		
+		historico.setBounds(515, 470, 0, 0);
+		historico.setSize(new Dimension(475, 240));
+		
+		home.add(visaoGeral);
+		home.add(transferencias);
+		home.add(historico);
+		home.add(fundo);
+		
+	}
 	
 	void janelaHome() {
 		
@@ -25,6 +61,8 @@ public class Home extends JFrame implements ActionListener {
 		//Capturando o evento do botao
 		sair.addActionListener(this);
 		
+		HomeDesing();
+		
 		//Definicios da janela HOME
 		home.setSize(1024, 768);
 		home.setVisible(true);
@@ -33,8 +71,10 @@ public class Home extends JFrame implements ActionListener {
 		home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//Funcionalidades abaixo
 		home.add(sair);
+		
+		
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
